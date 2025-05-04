35626a368696c297a3a2f9514135397a5887909f
Max Eriksson <teratoscincus@hotmail.com>
2025-05-04 13:30:07 +0200

    Set up Spring

---
+++ src/main/java/com/maxeriksson/lab_02/WebShopRestClientCliApplication.java
@@ -0,0 +1,12 @@
+package com.maxeriksson.lab_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopRestClientCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebShopRestClientCliApplication.class, args);
+    }
+}
